fce1eb03bc38e68ccb9838d3467c65d88246922d
Mon Inson <66734118+monaxx@users.noreply.github.com>
2023-07-21 01:31:22 +0800

    Add helper functions
    -playRound():   output result of a single round
    -game():        prompt user input and generate computer choice for
                    a single round

--- js/myJavaScriptFile.js
+++ js/myJavaScriptFile.js
@@ -10,3 +10,30 @@ function getComputerChoice(){
             return 'Scissors';
     }
 }
+
+function playRound(playerSelection, computerSelection){
+    let playerSelectionUpperCase = playerSelection.toUpperCase();
+    let computerSelectionUpperCase = computerSelection.toUpperCase();
+
+    if(playerSelectionUpperCase == computerSelectionUpperCase){
+        return 'Tie!'
+    }else if((playerSelectionUpperCase == 'ROCK' && computerSelectionUpperCase == 'SCISSORS') ||
+            (playerSelectionUpperCase == 'PAPER' && computerSelectionUpperCase == 'ROCK') ||
+            (playerSelectionUpperCase == 'SCISSORS' && computerSelectionUpperCase == 'PAPER')){
+        return `You win! ${playerSelectionUpperCase} beats ${computerSelectionUpperCase}`;
+    }else{
+        return `You lose! ${computerSelectionUpperCase} beats ${playerSelectionUpperCase}`;
+    }
+}
+
+function game(){
+    const playerSelection = window.prompt('Player choice:');
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+}
+
+game();
+game();
+game();
+game();
+game();
